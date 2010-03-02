86004c338cbebf3bf02b7759a816bb523cb6ba15
George Chatzigeorgiou <george@skroutz.gr>
2010-03-02 16:31:48 +0200

    Back to active support layout

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
-require 'active_support'
+require 'active_support/cache/blackhole_store'
 require 'blackhole_store'
 require 'spec'
 require 'spec/autorun'

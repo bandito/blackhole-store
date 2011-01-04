3a274874577d0f4e8345c6f656d7423dce6d62b8
Christos Trochalakis <yatiohi@ideopolis.gr>
2011-01-04 11:32:30 +0200

    add Rails2/Rails3 check and implement rails3 api

--- lib/active_support/cache/blackhole_store.rb
+++ lib/active_support/cache/blackhole_store.rb
@@ -1,42 +1,57 @@
+module Blackhole
+  module Rails2
+
+    def read(key, options = nil)
+      log("Read", key, options)
+      nil
+    end
+
+    def write(key, value, options = nil)
+      log("Write", key, options)
+      value
+    end
+
+    def delete(key, options = nil)
+      log("Delete", key, options)
+      false
+    end
+
+    def delete_matched(matcher, options = nil)
+    end
+
+    def exist?(key, options = nil)
+      false
+    end
+
+  end
+
+  module Rails3
+    protected
+    def read_entry(key, options)
+      nil
+    end
+
+    def write_entry(key, entry, options)
+      entry
+    end
+  end
+
+  module Store
+    include Rails::VERSION::MAJOR == 3 ? Rails3 : Rails2
+  end
+end
+
 module ActiveSupport
   module Cache
     class BlackholeStore < Store
-    
-      def initialize
-        super
+      include ::Blackhole::Store
+    end
 
-        @data = {}
-      end
-      
-      def read(key, options = nil)
-        log("Read", key, options)
-        nil
-      end
-  
-      def write(key, value, options = nil)
-        log("Write", key, options)
-        value
-      end
-  
-      def delete(key, options = nil)
-        log("Delete", key, options)
-        false
-      end
-  
-      def delete_matched(matcher, options = nil)
-      end
-  
-      def exist?(key, options = nil)    
-         false
-      end
-      
-      def clear
-      end
+    def initialize(options=nil)
+      options ||= {}
+      super(options)
+    end
 
-      def keys
-        @data.keys
-      end
-      
-    end    
+    def clear; end
   end
 end
